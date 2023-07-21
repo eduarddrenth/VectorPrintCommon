f6ff9aaf1a420e5e541421a9a3553bb1ac435b82
eduarddrenth <eduarddrenth@gmail.com>
2023-07-21 11:59:09 +0200

    request helper

--- src/main/java/com/vectorprint/RequestHelper.java
+++ src/main/java/com/vectorprint/RequestHelper.java
@@ -15,6 +15,26 @@
  */
 package com.vectorprint;
 
+/*-
+ * #%L
+ * VectorPrintCommon
+ * %%
+ * Copyright (C) 2011 - 2023 E. Drenth Software
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.net.HttpURLConnection;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -36,8 +56,23 @@ import org.slf4j.LoggerFactory;
 public class RequestHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(RequestHelper.class);
     
-    private static final HttpClient httpClient = HttpClient.newBuilder().build();
+    private final HttpClient httpClient;
 
+    /**
+     * provide your own httpclient that will be (re)used by this helper;
+     * @param httpClient 
+     */
+    public RequestHelper(HttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    /**
+     * Initialize this helper with a default httpclient.
+     */
+    public RequestHelper() {
+        this(HttpClient.newBuilder().build());
+    }
+   
 
     /**
      * Calls {@link #request(int, java.net.http.HttpRequest...) }
